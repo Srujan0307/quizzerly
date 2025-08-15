c32e42a82b68d37b3b9372b2ab6a2b4542a33518
Srujan <srujannjadhav@gmail.com>
2025-08-15 19:16:53 +0530

    Add auth layout to disable prerendering for login/register

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'default-no-store';
+
+export default function AuthLayout({ children }) {
+  return children;
+}
+
+
